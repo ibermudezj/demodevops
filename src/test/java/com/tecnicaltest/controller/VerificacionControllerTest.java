eee2bcc6f435358db493de4d1e07692cdb4cbdab
ibermudezj <ibermudezj@gmail.com>
2018-09-20 21:14:29 -0500

    cubiendo test automático - RetoDevOps BBVA

---
+++ src/test/java/com/tecnicaltest/controller/VerificacionControllerTest.java
@@ -0,0 +1,12 @@
+package com.tecnicaltest.controller;
+
+import org.junit.Test;
+
+public class VerificacionControllerTest {
+	
+	@Test	
+	public void test1 () {
+		String bbva = null;
+		System.out.println(bbva.toLowerCase());
+	}
+}
