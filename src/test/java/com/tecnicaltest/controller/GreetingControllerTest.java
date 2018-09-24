850906a8dfe461fc0a23d20cef22b6f24943081c
ibermudezj <ibermudezj@gmail.com>
2018-09-24 11:33:08 -0500

    insertando error al código

--- src/test/java/com/tecnicaltest/controller/GreetingControllerTest.java
+++ src/test/java/com/tecnicaltest/controller/GreetingControllerTest.java
@@ -7,6 +7,6 @@ public class GreetingControllerTest {
 	@Test	
 	public void test1 () {
 		String jeru = "esta haciendo pruebas";
-		System.out.println(jeru.toLowerCase());
+		System.out.println(jeru.toLowerCase());z
 	}
 }
